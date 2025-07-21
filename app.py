f3cc3fc12f793e9d0e3d75061060df741021888d
aspanyndr <aspanyundar20@gmail.com>
2025-07-21 18:32:37 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/project')
+def projects():
+    return render_template('project.html')
+
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
